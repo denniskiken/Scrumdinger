2aa02576e2592115b326f5cec3ae18513e5b1bf6
Dennis Kikendall <denniskiken@gmail.com>
2023-09-12 15:49:37 -0400

    m

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -1,3 +1,4 @@
+
 //
 //  Theme.swift
 //  Scrumdinger
--- Scrumdinger/ThemeView.swift
+++ Scrumdinger/ThemeView.swift
@@ -11,12 +11,17 @@ struct ThemeView: View {
     let theme: Theme
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: .infinity)
+            .background(theme.mainColor)
+            .foregroundColor(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
     }
 }
 
 struct ThemeView_Previews: PreviewProvider {
     static var previews: some View {
-        ThemeView(theme: .buttercup)
+        ThemeView(theme: .bubblegum)
     }
 }
